2ec468d43c6068b182f5d9c0d04c79160ccdf8f3
Christoph Meyer <christoph@chmey.com>
2020-11-16 19:12:25 +0100

    remove migrate and sqlalchemy

--- kanweek/__init__.py
+++ kanweek/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
-db = SQLAlchemy()
-migrate = Migrate(app, db)
 
 
 from . import routes # noqa

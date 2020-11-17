28edaee2c737765b594a57ca6cc4e566fef1f110
Christoph Meyer <christoph@chmey.com>
2020-11-17 19:40:53 +0100

    LabelSchema

--- kanweek/models/label.py
+++ kanweek/models/label.py
@@ -1,9 +1,19 @@
-from kanweek.extensions import db
+from kanweek.extensions import db, ma
+from marshmallow import fields
+from datetime import datetime
 
 
 class Label(db.Document):
-    name = db.StringField()
-    created_date = db.DateTimeField()
-    modified_date = db.DateTimeField()
-    user = db.DocumentField("User")
-    tasks = db.ListField(db.DocumentField("Task"))
+    name = db.StringField(required=True)
+    modified_date = db.DateTimeField(required=False, default=datetime.utcnow())
+    owner = db.ReferenceField("User")
+    tasks = db.ListField(db.ReferenceField("Task"))
+
+
+class LabelSchema(ma.Schema):
+    id = fields.Str()
+    name = fields.Str()
+    modified_date = fields.DateTime()
+
+    class Meta:
+        fields = ("id", "name", "modified_date")

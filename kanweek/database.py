d53a5ae6dd4bd148948f139dbaed22b3110890ed
Christoph Meyer <christoph@chmey.com>
2020-11-17 19:40:41 +0100

    drop unneeded files

--- kanweek/database.py
+++
@@ -1,2 +0,0 @@
-from flask_mongoalchemy import MongoAlchemy
-db = MongoAlchemy()
--- kanweek/models/weekday.py
+++
@@ -1,7 +0,0 @@
-from kanweek.extensions import db
-
-
-class Weekday(db.Document):
-    __tablename__ = 'weekday'
-    name = db.StringField()
-    tasks = db.ListField(db.DocumentField("Task"))
